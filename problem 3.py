f6c8a9b744065db841bad99061027af9b4dad367
Family <albon.136546120855@2depedqc.ph>
2023-04-29 01:45:00 +0800

    Program is tested and it is successful!

--- problem 3.py
+++ problem 3.py
@@ -9,17 +9,18 @@ print("\033[91m=" * 42)
 # pseudocode
 # open, create a text file to store the input lines
 with open("mylife.txt", "w") as problem_three_lines_file:
-     # ask the user for an input that will serve as context lines
-     # establish a loop that ask the user for a line until there is none
-     while True:
+    # ask the user for an input that will serve as context lines
+    # establish a loop that ask the user for a line until there is none
+    while True:
          my_line = input("\033[93mEnter a line: ")
-     # write the input line to the file
-     problem_three_lines_file.write(my_line + "\n")
-     # ask the user if there is more lines to input
-     add_line = input("\033[95Are there more lines y/n?: ")
-     # if the user enter n, exit the loop
-     if add_line.lower() == "n"
-         break
+         # write the input line to the file
+         problem_three_lines_file.write(my_line + "\n")
+         # ask the user if there is more lines to input
+         add_line = input("\033[96mAre there more lines y/n?: ")
+         # if the user enter n, exit the loop
+         if add_line.lower() == "n":
+             break
+
 # print the lines entered to the created file
 print("\033[92m~" * 60)
 print("\033[94mGreat! Your text file named mylife.txt is now saved and created.")
