4c0cbf5c00bdd72ffe8ea5a56199b26ef13463cd
Family <albon.136546120855@2depedqc.ph>
2023-04-29 01:19:02 +0800

    Instruction

--- problem 3.py
+++ problem 3.py
@@ -3,3 +3,6 @@ print("\033[91m=" * 42)
 print("\033[94mCMPE-103-MODULE-2-FILE-HANDLING-IN-PYTHON")
 print("\033[91m=" * 42)
 
+# writing a method in python where the user can write a multiple line of context
+# and will store into a text file named "mylife.txt"
+
